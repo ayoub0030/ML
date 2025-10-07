f05a22808780b5e0283bfa6db51a1c68e692612b
ayoub0030 <ayoub.elmeo@gmail.com>
2025-10-07 22:42:26 +0100

    Basic exploration

--- main.py
+++ main.py
@@ -3,3 +3,10 @@ import numpy as np
 from sklearn.datasets import fetch_california_housing
 import matplotlib.pyplot as plt
 import seaborn as sns
+
+data = fetch_california_housing(as_frame=True)
+df = data.frame  # Convert to a pandas DataFrame
+
+print("Shape of the dataset:", df.shape)
+print("\nFirst 5 rows:")
+print(df.head())
