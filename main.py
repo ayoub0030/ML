6dc6895a40866de392eeab083ea18e0138438b1e
ayoub0030 <ayoub.elmeo@gmail.com>
2025-10-08 15:10:01 +0100

    using . interactive python note book   ioynb

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
+# %%
 import pandas as pd
 import numpy as np
 from sklearn.datasets import fetch_california_housing
 import matplotlib.pyplot as plt
 import seaborn as sns
+# %%
 
 data = fetch_california_housing(as_frame=True)
 df = data.frame  # Convert to a pandas DataFrame
-"""
+
 print("Shape of the dataset:", df.shape)
 print("\nFirst 5 rows:")
 print(df.head())
@@ -34,8 +36,7 @@ plt.show()
 plt.figure(figsize=(10,8))
 sns.heatmap(df.corr(), annot=True, cmap='coolwarm')
 plt.title('Feature Correlation Heatmap')
-plt.show() """
-
+plt.show() 
 
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
@@ -71,7 +72,6 @@ y_pred = knn.predict(X_test_scaled)
 rmse = np.sqrt(mean_squared_error(y_test, y_pred))
 mae = mean_absolute_error(y_test, y_pred)
 r2 = r2_score(y_test, y_pred)
-
 print("KNN Model Performance:")
 print(f"RMSE: {rmse:.3f}")
 print(f"MAE:  {mae:.3f}")
@@ -86,3 +86,4 @@ plt.xlabel("Actual House Values")
 plt.ylabel("Predicted House Values")
 plt.title("KNN Predictions vs Actual Values")
 plt.show()
+
